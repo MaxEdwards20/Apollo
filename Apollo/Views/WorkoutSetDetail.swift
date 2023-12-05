a7f274405dc2689c37987f7081aeb075a9d2f011
Max Edwards <maxedwards20@gmail.com>
2023-12-05 14:40:22 -0700

    Adds navigation link to all exercise lists

---
+++ Apollo/Views/WorkoutSetDetail.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutSetDetail.swift
+//  Apollo
+//
+//  Created by Max Edwards on 12/5/23.
+//
+
+import SwiftUI
+
+struct WorkoutSetDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutSetDetail()
+}
